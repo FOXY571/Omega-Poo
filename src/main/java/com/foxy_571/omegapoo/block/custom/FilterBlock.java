ab68ff004e9427677abfdd3f433ab7078950328c
FOXY_571 <thomastowson2007@gmail.com>
2024-09-21 11:33:04 -0500

    Made filter block take in and out an item when used

--- src/main/java/com/foxy_571/omegapoo/block/custom/FilterBlock.java
+++ src/main/java/com/foxy_571/omegapoo/block/custom/FilterBlock.java
@@ -6,8 +6,11 @@ import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.InteractionResult;
+import net.minecraft.world.Containers;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.*;
@@ -48,22 +51,49 @@ public class FilterBlock extends HorizontalDirectionalBlock implements EntityBlo
     }
 
     @Override
-    protected @NotNull InteractionResult useWithoutItem(@NotNull BlockState state, Level level, @NotNull BlockPos pos, @NotNull Player player, @NotNull BlockHitResult hitResult) {
+    protected @NotNull ItemInteractionResult useItemOn(@NotNull ItemStack stack, @NotNull BlockState state, Level level, @NotNull BlockPos pos, @NotNull Player player, @NotNull InteractionHand hand, @NotNull BlockHitResult hitResult) {
         if (!level.isClientSide()) {
             BlockEntity blockEntity = level.getBlockEntity(pos);
             if (blockEntity instanceof FilterBlockEntity filterBlockEntity) {
-                int counter = filterBlockEntity.incrementCounter();
-                player.sendSystemMessage(Component.literal("BlockEntity has been used %d times".formatted(counter)));
+                ItemStack item = filterBlockEntity.getItem(0);
+                if (stack.isEmpty()) {
+                    if (item.isEmpty()) {
+                        player.sendSystemMessage(Component.literal("No item in filter block!"));
+                        return ItemInteractionResult.SUCCESS;
+                    }
 
-                return InteractionResult.sidedSuccess(level.isClientSide());
+                    player.setItemInHand(hand, filterBlockEntity.getItem(0).split(1));
+                }
+                else if (item.isEmpty()) {
+                    filterBlockEntity.setItem(0, player.isCreative() ? stack.copyWithCount(1) : stack.split(1));
+                }
+
+                return ItemInteractionResult.SUCCESS;
             }
         }
 
-        return super.useWithoutItem(state, level, pos, player, hitResult);
+        return ItemInteractionResult.sidedSuccess(level.isClientSide());
     }
 
     @Override
     public @Nullable BlockState getStateForPlacement(@NotNull BlockPlaceContext context) {
         return defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    protected void onRemove(@NotNull BlockState state, @NotNull Level level, @NotNull BlockPos pos, @NotNull BlockState newState, boolean isMoving) {
+        if (!state.is(newState.getBlock())) {
+            BlockEntity blockentity = level.getBlockEntity(pos);
+            if (blockentity instanceof FilterBlockEntity filterBlockEntity) {
+                if (!level.isClientSide()) {
+                    Containers.dropContents(level, pos, filterBlockEntity);
+                }
+
+                super.onRemove(state, level, pos, newState, isMoving);
+                level.updateNeighbourForOutputSignal(pos, this);
+            } else {
+                super.onRemove(state, level, pos, newState, isMoving);
+            }
+        }
     }
 }
--- src/main/java/com/foxy_571/omegapoo/block/entity/FilterBlockEntity.java
+++ src/main/java/com/foxy_571/omegapoo/block/entity/FilterBlockEntity.java
@@ -3,13 +3,18 @@ package com.foxy_571.omegapoo.block.entity;
 import com.foxy_571.omegapoo.OmegaPoo;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.NonNullList;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.Container;
+import net.minecraft.world.ContainerHelper;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
 
-public class FilterBlockEntity extends BlockEntity {
-    private int counter;
+public class FilterBlockEntity extends BlockEntity implements Container {
+    protected NonNullList<ItemStack> items = NonNullList.withSize(1, ItemStack.EMPTY);
 
     public FilterBlockEntity(BlockPos pos, BlockState blockState) {
         super(ModBlockEntities.FILTER.get(), pos, blockState);
@@ -20,7 +25,7 @@ public class FilterBlockEntity extends BlockEntity {
         super.loadAdditional(tag, registries);
 
         CompoundTag omegapooData = tag.getCompound(OmegaPoo.MOD_ID);
-        this.counter = omegapooData.getInt("Counter");
+        ContainerHelper.loadAllItems(omegapooData, this.items, registries);
     }
 
     @Override
@@ -28,13 +33,54 @@ public class FilterBlockEntity extends BlockEntity {
         super.saveAdditional(tag, registries);
 
         CompoundTag omegapooData = new CompoundTag();
-        omegapooData.putInt("Counter", this.counter);
+        ContainerHelper.saveAllItems(omegapooData, this.items, registries);
         tag.put(OmegaPoo.MOD_ID, omegapooData);
     }
 
-    public int incrementCounter() {
-        this.counter++;
-        setChanged();
-        return counter;
+    @Override
+    public int getContainerSize() {
+        return items.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return items.isEmpty();
+    }
+
+    @Override
+    public @NotNull ItemStack getItem(int slot) {
+        return items.get(slot);
+    }
+
+    @Override
+    public @NotNull ItemStack removeItem(int slot, int amount) {
+        ItemStack itemstack = ContainerHelper.removeItem(this.items, slot, amount);
+        if (!itemstack.isEmpty()) {
+            this.setChanged();
+        }
+
+        return itemstack;
+    }
+
+    @Override
+    public @NotNull ItemStack removeItemNoUpdate(int slot) {
+        return ContainerHelper.takeItem(this.items, slot);
+    }
+
+    @Override
+    public void setItem(int slot, @NotNull ItemStack stack) {
+        this.items.set(slot, stack);
+        stack.limitSize(1);
+        this.setChanged();
+    }
+
+    @Override
+    public boolean stillValid(@NotNull Player player) {
+        return Container.stillValidBlockEntity(this, player);
+    }
+
+    @Override
+    public void clearContent() {
+        items.clear();
     }
 }
